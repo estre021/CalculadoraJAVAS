f76ec4eca2061aba7116081383134c6bc47ce56f
estrella <estrellafriasmejia@gmail.com>
2025-12-03 17:20:21 -0400

    Agregadas mejoras de input y prompts

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-
 const TIEMPO_COCCION = 40;
 
 document.getElementById("btnCalcular").addEventListener("click", function () {
 
-    let capasInput = document.getElementById("capas");
-    let hornoInput = document.getElementById("horno");
+    let capasPrompt = prompt("¿Cuántas capas de lasaña estás preparando?");
+    let hornoPrompt = prompt("¿Cuántos minutos lleva la lasaña en el horno?");
 
-    let capas = capasInput.value;
-    let hornoActual = hornoInput.value;
-
-    if (capas === "" || hornoActual === "") {
-        alert("Debe completar todos los campos.");
+    if (capasPrompt === null || hornoPrompt === null || capasPrompt === "" || hornoPrompt === "") {
+        alert("Debe ingresar valores válidos.");
         return;
     }
 
-    capas = Number(capas);
-    hornoActual = Number(hornoActual);
+    let capas = Number(capasPrompt);
+    let hornoActual = Number(hornoPrompt);
 
     if (isNaN(capas) || isNaN(hornoActual) || capas < 0 || hornoActual < 0) {
         alert("Valores inválidos. No se permiten negativos ni letras.");
@@ -32,18 +28,22 @@ document.getElementById("btnCalcular").addEventListener("click", function () {
         mensajeRestante = "Tiempo restante en el horno: " + tiempoRestante + mensajeRestante + " minutos.";
     }
 
+
     let tiempoPreparacion = capas * 2;
+
     let tiempoTotal = hornoActual + tiempoPreparacion;
 
     document.getElementById("restante").textContent = mensajeRestante;
+
     document.getElementById("preparacion").textContent =
         "Tiempo total de preparación: " + tiempoPreparacion + " minutos.";
+
     document.getElementById("total").textContent =
         "Tiempo total de trabajo: " + tiempoTotal + " minutos.";
 
-    capasInput.value = "";
-    hornoInput.value = "";
-
-    
-    capasInput.focus();
+    document.getElementById("capas").value = capas;
+    document.getElementById("horno").value = hornoActual;
+    document.getElementById("capas").value = "";
+    document.getElementById("horno").value = "";
+    document.getElementById("capas").focus();
 });

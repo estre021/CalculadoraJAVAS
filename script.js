273eb7efef2f2ad2e29ecb0be378a0072045ab7d
estrella <estrellafriasmejia@gmail.com>
2025-12-03 16:42:22 -0400

    Añadida calculadora de lasaña

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-document.getElementById("btnIniciar").addEventListener("click", function () {
-let nombre = prompt("¿Cuál es tu nombre?");
-let nacimiento = prompt("¿En qué año naciste?");
+const TIEMPO_COCCION = 40;
 
-nacimiento = Number(nacimiento);
+document.getElementById("btnCalcular").addEventListener("click", function () {
 
-const anioActual = 2025;
+    let capas = prompt("¿Cuántas capas de lasaña estás preparando?");
+    let hornoActual = prompt("¿Cuántos minutos lleva la lasaña en el horno?");
 
-let edad = anioActual - nacimiento;
+    if (!capas || !hornoActual) {
+        alert("Debes ingresar valores válidos.");
+        return;
+    }
 
-let mensaje = "Hola, " + nombre + ". Tienes " + edad + " años.";
+    capas = Number(capas);
+    hornoActual = Number(hornoActual);
 
-document.getElementById("resultado").textContent = mensaje;
+    if (isNaN(capas) || isNaN(hornoActual) || capas < 0 || hornoActual < 0) {
+        alert("Los valores no pueden ser negativos ni contener letras.");
+        return;
+    }
+
+    let tiempoRestante = TIEMPO_COCCION - hornoActual;
+    let mensajeRestante = "";
+
+    if (tiempoRestante < 0) {
+        mensajeRestante = "Te excediste del tiempo de cocción.";
+        tiempoRestante = 0; 
+    } else {
+        mensajeRestante = "Tiempo restante en el horno: " + tiempoRestante + mensajeRestante + " minutos.";
+    }
+
+    let tiempoPreparacion = capas * 2;
+
+    let tiempoTotal = hornoActual + tiempoPreparacion;
+
+    document.getElementById("restante").textContent = mensajeRestante;
+
+    document.getElementById("preparacion").textContent =
+        "Tiempo total de preparación: " + tiempoPreparacion + " minutos.";
+
+    document.getElementById("total").textContent =
+        "Tiempo total de trabajo: " + tiempoTotal + " minutos.";
 });
